84ad7bec6c5c70dd104b2e4400430d2039cdea50
Jessica Guo <jessicaguo@email.arizona.edu>
2022-01-06 19:28:43 +0000

    Modify output org script to accomodate 3 treatments and 8 variables

--- temp_comparison/scripts/organize_output.R
+++ temp_comparison/scripts/organize_output.R
@@ -10,32 +10,30 @@ library(tidyr)
 # And 9 variables
 # 3 C fluxes (GPP, NPP, AutoResp) [kg C m^-2 s^-1]
 # 3 C pools (TotLivBiom, AGB, LAI) [kg C m^-2 or m^2 m^-2]
-# 3 energy/water fluxes (stomatal_conductance, Evap, Transp) [kg m^-2 s^-1]
-# (Is Transp == TVeg?)
+# 2 energy/water fluxes (Evap, Transp) [kg m^-2 s^-1]
+# (Evap = ET, so Transp/Evap is E/ET)
+
 
 treatments <- c("rn", "hn", "hnrn")
+variables <- c("GPP", "NPP", "AutoResp",
+               "TotLivBiom", "AGB", "LAI", 
+               "Evap", "Transp")
 
-# Separate lists of ensembles variables vs. sensitivity variables
-ens.variables <- c("GPP", "NPP", "AutoResp",
-                   "TotLivBiom", "AGB", "LAI", 
-                   "stomatal_conductance", "Evap", "Transp")
-sens.variables <- c("AGB", "TVeg")
 
 # Functions for conversion of biomass and transpiration units
 convert_units <- function(x, variable) {
-  if (variable %in% c("GPP", "NPP", "AutoResp")) {
-    # Convert to kg/m2/h
+  if(variable %in% c("GPP", "NPP", "AutoResp")) {
+    # Convert to kg C /m2/h
     return(ud.convert(x, "kg/m2/s", "kg/m2/h"))
-  } else if (variable %in% c("TotLivBiom", 
-                             "AGB")) {
-    # undo  biomass to C conversion in PEcAn, already converted to kg/m2
+  } else if(variable %in% c("TotLivBiom", "AGB")) {
+    # undo biomass to C conversion in PEcAn, kg biomass /m2
     return(x / 0.4)
-  } else if (variable %in% c("stomatal_conductance", 
-                             "Evap", 
-                             "TVeg",
+  } else if(variable %in% c("Evap", 
                              "Transp")) {
     # Convert to kg/m2/h
     return(ud.convert(x, "kg/m2/s", "kg/m2/h"))
+  } else if(variable == "LAI") { # no conversion needed for LAI
+    return(x)
   }
 }
 
@@ -47,14 +45,14 @@ for(trt in treatments){
   timescale <- data.frame(day = rep(biocro_result$doy, each = 24), hour = 0:23)
   rm(biocro_result)
   
-  for(v in ens.variables){
+  for(v in variables){
     
     # Load in wide format of ensemble outputs
     load(paste0("/data/output/pecan_runs/temp_comp_ms/", trt, 
                 "/ensemble.ts.NOENSEMBLEID.", v, ".2019.2019.Rdata"))
     
     # Rearrange to long format and summarize across ensembles
-    if (v %in% c("TotLivBiom", "AGB", "LAI")) { # Pools, take midday values only
+    if (v %in% c("TotLivBiom", "AGB", "LAI")) { # Pools or ratios, take midday values only
       daily <- data.frame(timescale, t(ensemble.ts[[v]])) %>% 
         pivot_longer(cols = starts_with("X"), names_to = "ensemble",
                      names_prefix = "X", values_to = "output") %>% 
@@ -69,7 +67,7 @@ for(trt in treatments){
                   lcl_95 = quantile(output, probs = c(0.025), na.rm = TRUE), 
                   ucl_95 = quantile(output, probs = c(0.975), na.rm = TRUE))
     } else if (v %in% c("GPP", "NPP", "AutoResp", 
-                        "stomatal_conductance", "Evap", "Transp")) { # Fluxes, take daily sums first
+                        "Evap", "Transp")) { # Fluxes, take daily sums first
       daily <- data.frame(timescale, t(ensemble.ts[[v]])) %>%
         pivot_longer(cols = starts_with("X"), names_to = "ensemble",
                      names_prefix = "X", values_to = "output") %>%
@@ -97,8 +95,11 @@ for(trt in treatments){
 
 
 # Summarize pair-wise differences across treatments, by ensemble
+# hn_rn = high night - regular night, effect of different params and temps
+# hnrn_rn = high night control temp - regular night, effect of high temp alone
+# hn_hnrn = high night - high night control temp, effect of high temp params alone
 
-for(v in ens.variables){
+for(v in variables){
   
   # Load in daily summary of median biocro output, to obtain correct timestamps
   load(paste0("/data/output/pecan_runs/temp_comp_ms/rn/out/SA-median/biocro_output.RData"))
@@ -108,7 +109,7 @@ for(v in ens.variables){
   # Load all 3 treatments, summarize to daily depending on variable
   load(paste0("/data/output/pecan_runs/temp_comp_ms/rn/ensemble.ts.NOENSEMBLEID.", 
               v, ".2019.2019.Rdata"))
-  if (v %in% c("TotLivBiom", "AGB")) {
+  if (v %in% c("TotLivBiom", "AGB", "LAI")) {
     rn <- data.frame(timescale, t(ensemble.ts[[v]])) %>%
       pivot_longer(cols = starts_with("X"), names_to = "ensemble",
                    names_prefix = "X", values_to = "output") %>% 
@@ -117,7 +118,8 @@ for(v in ens.variables){
       filter(hour == 12) %>% 
       group_by(day, ensemble) %>%
       dplyr::select(-hour)
-  } else if (v == "TVeg") {
+  } else if (v %in% c("GPP", "NPP", "AutoResp", 
+                      "Evap", "Transp")) {
     rn <- data.frame(timescale, t(ensemble.ts[[v]])) %>%
       pivot_longer(cols = starts_with("X"), names_to = "ensemble",
                    names_prefix = "X", values_to = "output") %>%
@@ -130,7 +132,7 @@ for(v in ens.variables){
   
   load(paste0("/data/output/pecan_runs/temp_comp_ms/hn/ensemble.ts.NOENSEMBLEID.", 
               v, ".2019.2019.Rdata"))
-  if (v %in% c("TotLivBiom", "AGB")) {
+  if (v %in% c("TotLivBiom", "AGB", "LAI")) {
     hn <- data.frame(timescale, t(ensemble.ts[[v]])) %>%
       pivot_longer(cols = starts_with("X"), names_to = "ensemble",
                    names_prefix = "X", values_to = "output") %>% 
@@ -139,7 +141,8 @@ for(v in ens.variables){
       filter(hour == 12) %>% 
       group_by(day, ensemble)%>%
       dplyr::select(-hour)
-  } else if (v == "TVeg") {
+  } else if (v %in% c("GPP", "NPP", "AutoResp", 
+                      "Evap", "Transp")) {
     hn <- data.frame(timescale, t(ensemble.ts[[v]])) %>%
       pivot_longer(cols = starts_with("X"), names_to = "ensemble",
                    names_prefix = "X", values_to = "output") %>%
@@ -149,19 +152,47 @@ for(v in ens.variables){
       summarise(output = sum(output))
   }
   
+  load(paste0("/data/output/pecan_runs/temp_comp_ms/hnrn/ensemble.ts.NOENSEMBLEID.", 
+              v, ".2019.2019.Rdata"))
+  if (v %in% c("TotLivBiom", "AGB", "LAI")) {
+    hnrn <- data.frame(timescale, t(ensemble.ts[[v]])) %>%
+      pivot_longer(cols = starts_with("X"), names_to = "ensemble",
+                   names_prefix = "X", values_to = "output") %>% 
+      mutate(output = convert_units(output, variable = v),
+             ensemble = as.numeric(ensemble)) %>%
+      filter(hour == 12) %>% 
+      group_by(day, ensemble) %>%
+      dplyr::select(-hour)
+  } else if (v %in% c("GPP", "NPP", "AutoResp", 
+                      "Evap", "Transp")) {
+    hnrn <- data.frame(timescale, t(ensemble.ts[[v]])) %>%
+      pivot_longer(cols = starts_with("X"), names_to = "ensemble",
+                   names_prefix = "X", values_to = "output") %>%
+      mutate(output = convert_units(output, variable = v),
+             ensemble = as.numeric(ensemble)) %>%
+      group_by(day, ensemble) %>%
+      summarise(output = sum(output))
+  }
+  rm(ensemble.ts)
+  
   # Combine to single df and calculate differences across treatments
-  all <- bind_cols(rn, hn["output"]) %>%
+  all <- bind_cols(rn, hn["output"], hnrn["output"]) %>%
     rename(rn = 3,
-           hn = 4) %>%
+           hn = 4,
+           hnrn = 5) %>%
     # One-sided t-tests predictions
-    mutate(hn_rn = hn - rn)
+    mutate(hn_rn = hn - rn,
+           hnrn_rn = hnrn - rn,
+           hn_hnrn = hn - hnrn)
   
   # Summarize whether across ensembles, the differences are significant each day
   diff_stat <- all %>%
-    dplyr::select(-rn, -hn) %>%
+    dplyr::select(-rn, -hn, -hnrn) %>%
     group_by(day) %>%
     # Reports the 2.5, 5, 50, 95, and 97.5th percentile of each set of differences
-    summarize(hn_rn = quantile(hn_rn, probs = c(0.025, 0.05, 0.25, 0.5, 0.75, 0.95, 0.975))) %>%
+    summarize(hn_rn = quantile(hn_rn, probs = c(0.025, 0.05, 0.25, 0.5, 0.75, 0.95, 0.975)),
+              hnrn_rn = quantile(hnrn_rn, probs = c(0.025, 0.05, 0.25, 0.5, 0.75, 0.95, 0.975)),
+              hn_hnrn = quantile(hn_hnrn, probs = c(0.025, 0.05, 0.25, 0.5, 0.75, 0.95, 0.975))) %>%
     mutate(percentile = c("025", "050", "250", "500", "750", "950", "975")) %>%
     relocate(day, percentile)
   
@@ -172,7 +203,7 @@ for(v in ens.variables){
 
 # Collate variance decomposition variables across treatments
 
-for(v in sens.variables) {
+for(v in variables) {
   vd <- c()
   for(trt in treatments) {
     load(paste0("/data/output/pecan_runs/temp_comp_ms/", trt, 
